6070259720d5af30c6df571165108b3dd1aa26e4
cjbridges70 <47092424+cjbridges70@users.noreply.github.com>
2019-04-07 19:45:58 -0400

    Update DestroyByContact.cs

--- DestroyByContact.cs
+++ DestroyByContact.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Done_DestroyByContact : MonoBehaviour
+public class DestroyByContact : MonoBehaviour
 {
 	public GameObject explosion;
 	public GameObject playerExplosion;
